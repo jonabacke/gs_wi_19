7fb6534316f06d87f2b3e3a48956b7885adaa42c
acm064 <tobias.hardjowirogo@haw-hamburg.de>
2019-11-15 00:23:26 +0100

    24bit

--- BitMap/build24BitStruct.c
+++ BitMap/build24BitStruct.c
@@ -3,3 +3,45 @@
 //
 
 #include "build24BitStruct.h"
+#include "stdint.h"
+#include <stdio.h>
+#include "general.h"
+#include "output.h"
+#include <malloc.h>
+
+uint8_t build24BitPictureArray(struct tagBitMap24Bit *picture24Bit, uint8_t *buffer) {
+
+    //Bildhoehe und -breite aus infoHeader auslesen
+    static int32_t width = 0;
+    static int32_t height = 0;
+    static int32_t buffersize = 0;
+
+    width = picture24Bit -> infoHeader.biWidth + 4 - picture24Bit -> infoHeader.biWidth % 4;
+    height = picture24Bit -> infoHeader.biHeight;
+    buffersize = sizeof(buffer)-54; //54 Byte vom Header
+
+    //Array vom Typ "struct tagRGBTriple" mit RGB-Werten aus dem Buffer fuellen
+    struct tagRGBTriple picRGB[buffersize/3];
+
+    picture24Bit -> pixel = (tagRGBTriple **)malloc(buffersize/3);
+    for (int i = 0; i < height; ++i) {
+        picture24Bit -> pixel[i] = (uint8_t*) malloc(width);
+    }
+
+    for (int32_t i = 0, j = 0; i < buffersize/3, j < buffersize; i++, j + 3) {
+        picRGB[i].rgbBlue = buffer[j];
+        picRGB[i].rgbGreen = buffer[j+1];
+        picRGB[i].rgbRed = buffer[j+2];
+    }
+
+    //picture24Bit (tagBitMap24Bit-struct) mit RGBTriple-Werten fuellen
+    for (int32_t i; i < sizeof(picRGB); i++) {
+        for (int32_t j = 0; j < height; j++) {
+            for (int32_t k = 0; k - width; k++) {
+                picture24Bit -> pixel[k][j] = picRGB[i];
+            }
+        }
+    }
+
+    return 0;
+}
--- BitMap/build24BitStruct.h
+++ BitMap/build24BitStruct.h
@@ -5,4 +5,9 @@
 #ifndef BITMAP_BUILD24BITSTRUCT_H
 #define BITMAP_BUILD24BITSTRUCT_H
 
+#include <stdint.h>
+#include "general.h"
+
+uint8_t build24BitPictureArray(struct tagBitMap24Bit *picture24Bit, uint8_t *buffer);
+
 #endif //BITMAP_BUILD24BITSTRUCT_H
