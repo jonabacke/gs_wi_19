1c0a2c23be38c4a5dde70ebdd92f2172f1203fbd
Jonathan <jonathan.backes.jb@googlemail.com>
2019-12-06 12:42:55 +0100

    listHeader

---
+++ 1-Wire/sensorList.h
@@ -0,0 +1,19 @@
+
+
+#include "stdint.h"
+
+
+
+uint8_t getTemperature();
+
+uint8_t setTemperature();
+
+uint8_t getRomCode();
+
+uint8_t setRomCode();
+
+uint8_t setNext();
+
+uint8_t getNext();
+
+uint8_t pullTemperature();
